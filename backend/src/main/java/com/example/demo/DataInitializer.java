737a218fd980b9c1aacdf5e549630e5740baa696
Erick Saraiva <erick.ssilva@bandtec.com.br>
2023-10-17 13:34:13 -0300

    adicionado a parte de seleção do carro do cliente para analise

--- backend/src/main/java/com/example/demo/DataInitializer.java
+++ backend/src/main/java/com/example/demo/DataInitializer.java
@@ -25,20 +25,26 @@ public class DataInitializer {
     @PostConstruct
     public void init() {
         // Inicializa dados para a entidade Cliente
-        Cliente cliente1 = new Cliente("João", "joao@example.com", "11 96732-8989");
-        Cliente cliente2 = new Cliente("Maria", "maria@example.com", "11 94567-3259");
-        Cliente cliente3 = new Cliente("Pedro", "pedro@example.com", "21 98752-7621");
+        Cliente cliente1 = new Cliente("João", "joao@email.com", "11 96732-8989");
+        Cliente cliente2 = new Cliente("Maria", "maria@email.com", "11 94567-3259");
+        Cliente cliente3 = new Cliente("Pedro", "pedro@email.com", "21 98752-7621");
         clienteRepository.save(cliente1);
         clienteRepository.save(cliente2);
         clienteRepository.save(cliente3);
 
         // Inicializa dados para a entidade Veiculo
-        Veiculo veiculo1 = new Veiculo(cliente1,"joao@example.com", "Toyota", "Corolla", 2018);
-        Veiculo veiculo2 = new Veiculo(cliente2,"maria@example.com", "Ford", "Fiesta", 2019);
-        Veiculo veiculo3 = new Veiculo(cliente3,"pedro@example.com", "Honda", "Civic", 2020);
-        veiculoRepository.save(veiculo1);
-        veiculoRepository.save(veiculo2);
-        veiculoRepository.save(veiculo3);
+        Veiculo veiculo1Joao = new Veiculo(cliente1,"joao@email.com", "Toyota", "Corolla", 2018);
+        Veiculo veiculo2Maria = new Veiculo(cliente2,"maria@email.com", "Ford", "Fiesta", 2017);
+        Veiculo veiculo3Pedro = new Veiculo(cliente3,"pedro@email.com", "Honda", "Civic", 2020);
+        Veiculo veiculo4Joao = new Veiculo(cliente3,"joao@email.com", "Hyundai", "Hb-20", 2022);
+        Veiculo veiculo5Joao = new Veiculo(cliente3,"joao@email.com", "Renault", "Clio", 2018);
+        Veiculo veiculo6Maria = new Veiculo(cliente3,"pedro@email.com", "BMW", "118i", 2020);
+        veiculoRepository.save(veiculo1Joao);
+        veiculoRepository.save(veiculo2Maria);
+        veiculoRepository.save(veiculo3Pedro);
+        veiculoRepository.save(veiculo4Joao);
+        veiculoRepository.save(veiculo5Joao);
+        veiculoRepository.save(veiculo6Maria);
 
         // Adicione mais inserções conforme necessário para outras entidades
     }
